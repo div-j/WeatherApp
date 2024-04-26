ecbe2948c3ef868e01c916cf36a46ed93caba7d9
div-j <divinejohn65@gmail.com>
2024-04-26 13:17:06 +0100

    files

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-let wp = document.getElementById("w-result");
 //getting the search button
 const searchButton = document.getElementById("search-button");
 //getting input search
@@ -17,28 +16,38 @@ function getWeather() {
     alert("Please enter a city name!");
     return;
   }
+  //api url
   const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}`;
 
+  //fetch api implementation
   fetch(apiUrl)
     .then((response) => response.json())
     .then((data) => {
+        //checking for status code
       if (data.cod === "404") {
-        weatherInfo.textContent = "City not found!";
+        weatherInfo.innerHTML = "City not found!";
       } else {
+
+        //getting weather variables
         console.log(data);
         const city = data.name;
         const temp = data.main.temp;
         const description = data.weather[0].description;
+        const speed = data.wind.speed
+
+        //displaying results to html
 
         weatherInfo.innerHTML = `
     <h2>Weather in ${city}</h2>
     <p>Temperature: ${temp}°C</p>
     <p>Description: ${description}</p>
+    <p>Wind speed: ${speed} m/s2</p>
   `;
       }
     })
+    //error handling
     .catch((err)=>{
-        weatherInfo.innerHTML=`<span id='error'>Oppps!! Something went wrong : ${err} </span>`
+        weatherInfo.innerHTML=`<span id='error'>Ooppps!! Something went wrong : ${err} </span>`
     })
     ;
 }
